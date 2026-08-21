10e5a4fdbe691b472cd86a9adf989b515d49540f
Dorian Gonzalez <doriandarren1@gmail.com>
2026-08-21 22:47:12 +0200

    fix laravel env

--- gen/php_laravel/to_create_project/start_project_php.py
+++ gen/php_laravel/to_create_project/start_project_php.py
@@ -1,12 +1,18 @@
+import os
+from dotenv import load_dotenv
 from gen.helpers.helper_menu import pause
 from gen.helpers.helper_print import input_with_validation
 from gen.services.generator_services import create_php_project_service
 
 
+load_dotenv()
+
 
 def start_project_php():
      # Defaults
-    default_path = "/Users/dorian/PHPProjects"
+    
+    #default_path = "/Users/dorian/PHPProjects"
+    default_path = os.getenv("DEFAULT_PATH_PHP")    
     default_name = "api.app1.com"
 
     # Inputs
